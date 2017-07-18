6acd582d439675c3fc1d54c7f0c66a368d52f966
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-07-18 20:31:47 +0200

    Create a copy of the GameRunner as a basis for a Golden Master.

---
+++ src/test/java/com/academicscode/trivia/GameRunnerGoldenMasterTest.java
@@ -0,0 +1,36 @@
+package com.academicscode.trivia;
+
+import java.util.Random;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+// Intentionally duplicates production GameRunner for now, to avoid changing
+// production code that somebody might depend on.
+public class GameRunnerGoldenMasterTest {
+
+    private static boolean notAWinner;
+
+    public static void main(String[] args) {
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random();
+
+        do {
+
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+        } while (notAWinner);
+
+    }
+}
